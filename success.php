75900b5f37eef7e8333ea1b962be99b7f41f333f
Eugene Primachenko <eugenii.primachenko.arpo@gmail.com>
2020-07-04 22:02:31 +0300

    add_reg

---
+++ success.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+if (isset($_SESSION['success'])) {
+
+    echo $_SESSION['success'] . '<br />' . '<br />' . '<br />';
+    echo 'publicKey - ' . $_SESSION['publicKey'] . '<br />' . '<br />';
+    echo 'privateKey - ' . $_SESSION['privateKey'] . '<br />' . '<br />';
+    echo 'Please save the keys. My account does not work yet!!!';
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+    <a href="store/index.html">List of module</a>
+
+    </body>
+    </html>
+
+
+    <?php
+}
